8ecc56ab887054398b732d70a5cafd4990c87314
Hugo Solleder <hugo.solleder@epfl.ch>
2024-11-07 11:30:13 +0100

    chore: add functions to pNeuma_simulator __init__

--- pNeuma_simulator/__init__.py
+++ pNeuma_simulator/__init__.py
@@ -1,6 +1,7 @@
 from .animations import draw, ring  # noqa F401
 from .identify import identify  # noqa F401
 from .infront import infront  # noqa F401
+from .results import loader, aggregate, intersect, normalized  # noqa F401
 from .simulate import main, batch, CollisionException  # noqa F401
 
 # from .results import loader, aggregate, intersect  # noqa F401
