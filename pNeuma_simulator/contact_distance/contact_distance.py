c681fe3e853a850cb00d953fd0bada4861722ef6
Hugo Solleder <hugo.solleder@epfl.ch>
2025-01-20 09:05:58 +0100

    refactor: switch scalar calls to math library for contact_distance
    Issue(s): #2

--- pNeuma_simulator/contact_distance/contact_distance.py
+++ pNeuma_simulator/contact_distance/contact_distance.py
@@ -1,3 +1,5 @@
+from math import atan2, cos, sin, sqrt
+
 import numpy as np
 from numba import jit
 
@@ -35,7 +37,7 @@ def calc_dtc(
     """
 
     # distance from i to j
-    s_i_j = np.sqrt((x_j - x_i) ** 2 + (y_j - y_i) ** 2)
+    s_i_j = sqrt((x_j - x_i) ** 2 + (y_j - y_i) ** 2)
     # distance of closest approach between i and j
     min_d = ellipses(l_j, w_j, l_i, w_i, x_j, y_j, x_i, y_i, theta_j, theta_i)
     dtc = s_i_j - min_d
@@ -78,43 +80,43 @@ def ellipses(
     Returns:
         float: distance between the centers when two ellipses are externally tangent
     """
-    theta3 = np.atan2(y2 - y1, x2 - x1)
-    cs1, sn1 = np.cos(theta1), np.sin(theta1)
-    cs3, sn3 = np.cos(theta3), np.sin(theta3)
-    k1d = np.cos(theta3 - theta1)
-    k2d = np.cos(theta3 - theta2)
-    k1k2 = np.cos(theta2 - theta1)
+    theta3 = atan2(y2 - y1, x2 - x1)
+    cs1, sn1 = cos(theta1), sin(theta1)
+    cs3, sn3 = cos(theta3), sin(theta3)
+    k1d = cos(theta3 - theta1)
+    k2d = cos(theta3 - theta2)
+    k1k2 = cos(theta2 - theta1)
     # eccentricity of ellipses
     e1 = 1 - (b1**2 / a1**2)
     e2 = 1 - (b2**2 / a2**2)
     # component of A'
     eta = a1 / b1 - 1
     a11 = b1**2 / b2**2 * (1 + 0.5 * (1 + k1k2) * (eta * (2 + eta) - e2 * (1 + eta * k1k2) ** 2))
-    a12 = b1**2 / b2**2 * 0.5 * np.sqrt(1 - k1k2**2) * (eta * (2 + eta) + e2 * (1 - eta**2 * k1k2**2))
+    a12 = b1**2 / b2**2 * 0.5 * sqrt(1 - k1k2**2) * (eta * (2 + eta) + e2 * (1 - eta**2 * k1k2**2))
     a22 = b1**2 / b2**2 * (1 + 0.5 * (1 - k1k2) * (eta * (2 + eta) - e2 * (1 - eta * k1k2) ** 2))
     # eigenvalues of A'
-    lambda1 = 0.5 * (a11 + a22) + 0.5 * np.sqrt((a11 - a22) ** 2 + 4 * a12**2)
-    lambda2 = 0.5 * (a11 + a22) - 0.5 * np.sqrt((a11 - a22) ** 2 + 4 * a12**2)
+    lambda1 = 0.5 * (a11 + a22) + 0.5 * sqrt((a11 - a22) ** 2 + 4 * a12**2)
+    lambda2 = 0.5 * (a11 + a22) - 0.5 * sqrt((a11 - a22) ** 2 + 4 * a12**2)
     # major and minor axes of transformed ellipse
-    b2p = 1 / np.sqrt(lambda1)
-    a2p = 1 / np.sqrt(lambda2)
+    b2p = 1 / sqrt(lambda1)
+    a2p = 1 / sqrt(lambda2)
     deltap = a2p**2 / b2p**2 - 1
     if abs(k1k2) == 1:
         if a11 > a22:
-            kpmp = 1 / np.sqrt(1 - e1 * k1d**2) * b1 / a1 * k1d
+            kpmp = 1 / sqrt(1 - e1 * k1d**2) * b1 / a1 * k1d
         else:
-            kpmp = (sn3 * cs1 - cs3 * sn1) / np.sqrt(1 - e1 * k1d**2)
+            kpmp = (sn3 * cs1 - cs3 * sn1) / sqrt(1 - e1 * k1d**2)
     elif deltap != 0:
         kpmp = (
-            a12 / np.sqrt(1 + k1k2) * (b1 / a1 * k1d + k2d + (b1 / a1 - 1) * k1d * k1k2)
-            + (lambda1 - a11) / np.sqrt(1 - k1k2) * (b1 / a1 * k1d - k2d - (b1 / a1 - 1) * k1d * k1k2)
-        ) / np.sqrt(2 * (a12**2 + (lambda1 - a11) ** 2) * (1 - e1 * k1d**2))
+            a12 / sqrt(1 + k1k2) * (b1 / a1 * k1d + k2d + (b1 / a1 - 1) * k1d * k1k2)
+            + (lambda1 - a11) / sqrt(1 - k1k2) * (b1 / a1 * k1d - k2d - (b1 / a1 - 1) * k1d * k1k2)
+        ) / sqrt(2 * (a12**2 + (lambda1 - a11) ** 2) * (1 - e1 * k1d**2))
     else:
         kpmp = 0
     if kpmp == 0 or deltap == 0:
         Rc = a2p + 1
         # The distance of closest approach
-        dist = Rc * b1 / np.sqrt(1 - e1 * k1d**2)
+        dist = Rc * b1 / sqrt(1 - e1 * k1d**2)
         return dist
     else:
         # coefficients of quartic for q
@@ -129,9 +131,9 @@ def ellipses(
         rts = np.real(np.roots(np.array([A, B, C, D, E], dtype=np.complex128)))
         qq = rts[rts > 0][0]
         # substitute for R'
-        Rc = np.sqrt(
+        Rc = sqrt(
             (qq**2 - 1) / deltap * (1 + b2p * (1 + deltap) / qq) ** 2 + (1 - (qq**2 - 1) / deltap) * (1 + b2p / qq) ** 2
         )
         # The distance of closest approach
-        dist = Rc * b1 / np.sqrt(1 - e1 * k1d**2)
+        dist = Rc * b1 / sqrt(1 - e1 * k1d**2)
         return dist
