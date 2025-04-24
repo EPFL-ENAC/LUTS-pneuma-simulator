c6019619c9bc4b05c3c71664ebf77bc532a00f75
GAnagno <georgios.anagnostopoulos@epfl.ch>
2025-04-24 17:22:27 +0200

    chore: change hpc params
    Issue(s): #7

--- hpc_script.sh
+++ hpc_script.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #SBATCH --job-name RUN_EXP
 #SBATCH --account=luts
-#SBATCH --nodes 4
+#SBATCH --nodes 1
 #SBATCH --partition=standard
-#SBATCH --qos=parallel
-#SBATCH --time 2-00:00:00
+#SBATCH --qos=serial
+#SBATCH --time 04:00:00
 #SBATCH --ntasks-per-node=1
 #SBATCH --output "out/slurm-%A_%a.log"
-#SBATCH --mem=128G
-#SBATCH --cpus-per-task=288
+#SBATCH --mem=64G
+#SBATCH --cpus-per-task=72
 
 module load gcc/13.2.0
 echo "${@:1}"
