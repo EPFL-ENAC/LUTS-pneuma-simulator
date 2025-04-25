2bfe89d38dfcb5169098a28179ba9f3007bec437
GAnagno <georgios.anagnostopoulos@epfl.ch>
2025-04-25 11:37:45 +0200

    fix: bigmem setting
    Issue(s): #7

--- hpc_script.sh
+++ hpc_script.sh
@@ -2,12 +2,12 @@
 #SBATCH --job-name RUN_EXP
 #SBATCH --account=luts
 #SBATCH --nodes 4
-#SBATCH --partition=standard
+#SBATCH --partition=bigmem
 #SBATCH --qos=parallel
 #SBATCH --time 06:00:00
 #SBATCH --ntasks-per-node=1
 #SBATCH --output "out/slurm-%A_%a.log"
-#SBATCH --mem=64G
+#SBATCH --mem=256G
 #SBATCH --cpus-per-task=72
 
 module load gcc/13.2.0
