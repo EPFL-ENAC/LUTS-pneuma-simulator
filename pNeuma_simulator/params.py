ef5b471ded9d14bb87da74cb78d1c8031a4f4ecb
GAnagno <georgios.anagnostopoulos@epfl.ch>
2026-03-27 16:08:55 +0100

    fix: virtual loop detectors
    Issue(s): #11

--- pNeuma_simulator/params.py
+++ pNeuma_simulator/params.py
@@ -7,7 +7,7 @@ factor = 3.6
 
 # Simulation parameters
 T = 180  # duration in seconds
-L = 8 * 360  # road length in meters
+L = 720  # 8 * 360  # 260  # 4 * 360  # road length in meters
 d_max = 45.0  # horizon
 clearance = 0.3  # dart spacing
 k = 300  # number of darts thrown
